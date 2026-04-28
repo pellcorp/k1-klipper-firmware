d5ad1e3eb34df789aae4ee808dc9fd2832100747
Jason Pell <jason@adnuntius.com>
2026-04-29 09:25:41 +1000

    adjustments to ender 3 v3 bed to include load cell stuff

--- _build.sh
+++ _build.sh
@@ -4,8 +4,15 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P)"
 cd $SCRIPT_DIR
 
+type=$(echo $1 | awk -F '-' '{print $1}')
+
+target=outfw
+if [ "$type" != "$1" ]; then
+  target=outfw/$type
+fi
+
 cp .config.$1 .config
-mkdir -p outfw/
+mkdir -p $target
 make clean
 make
-mv out/*000.bin outfw/
+mv out/*000.bin $target
